8d2549b2ff926d21fe921ffc70f53adf8e6bd24a
nyanko2309 <158505719+nyanko2309@users.noreply.github.com>
2024-03-02 19:34:17 +0200

    Add files via upload

---
+++ project/first.py
@@ -0,0 +1,13 @@
+x=int(input(" one side"))
+y=int(input("two side"))
+
+
+def tri_surf(x, y):
+    return ((x * y) / 2)
+
+print("s is,",tri_surf(x,y))
+
+def absolute(x):
+     if (x<=o): return (x*-1)
+     else:   return x
+     
