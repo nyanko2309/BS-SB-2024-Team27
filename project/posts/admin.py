355bf7103ed7f730bac46efdcf337fac4dea1e7b
Vitaly <you@example.com>
2024-03-08 09:56:26 +0200

    test

--- project/posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post
-admin.site.register(Post)
-
-from django.contrib import admin
-
-# Register your models here.
--- project/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Post
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['description', 'location', 'payment', 'work_hours', 'phys_lvl', 'kind_of_job', 'category']
