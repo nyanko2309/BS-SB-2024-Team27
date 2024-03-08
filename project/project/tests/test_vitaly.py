5a6634677c217cd4023d8186d3f0c44b4cb7d89e
Alon <alonil@ac.sce.ac.il>
2024-03-08 13:43:04 +0200

    test

--- project/project/tests/test_vitaly.py
+++ project/project/tests/test_vitaly.py
@@ -1,7 +1,7 @@
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from project.views import submit
-from views import login_button
+from project.views import login_button
 from django.test import Client
 from login.models import User
 from django.http import JsonResponse
@@ -15,15 +15,9 @@ class TestSite(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-    # run a unit test on
+
     def test_register_working(self):
-        data = {'mail': 'test@example.com', 'password': 'Aa123'}
+        data = {'mail': "test@example.com", 'password': "Aa123"}
         request = self.factory.post(reverse('submit'), data)
         response = submit(request)
         self.assertEqual(response.status_code, 302)
-
-    # def test_login_working(self):
-    #     data = {'mail': 'test@example.com', 'password': 'Aa123'}
-    #     request = self.factory.post(reverse('login_button'), data)
-    #     response = login_button(request)
-    #     self.assertEqual(response.status_code, 200)
